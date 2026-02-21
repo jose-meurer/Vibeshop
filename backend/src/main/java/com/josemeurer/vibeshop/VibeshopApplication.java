c865370f262664849f31ee3372fcc644ea9ec77f
Jose Meurer <josewmeurer@gmail.com>
2026-02-21 12:44:08 -0300

    Criado projeto inicial do backend

---
+++ backend/src/main/java/com/josemeurer/vibeshop/VibeshopApplication.java
@@ -0,0 +1,13 @@
+package com.josemeurer.vibeshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VibeshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VibeshopApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/josemeurer/vibeshop/VibeshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.josemeurer.vibeshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VibeshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
